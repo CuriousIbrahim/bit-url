c1aaa9d1fccf1d9e03e9cc2e0eae0ab914fd6f73
ibrahim <ibrahim74115@gmail.com>
2020-05-09 17:16:41 -0400

    Add flask-cors to all all requests, add route /<id>

--- biturl/app.py
+++ biturl/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify
+from flask_cors import CORS
 from datetime import datetime
 from threading import Thread
 
@@ -11,6 +12,7 @@ repository = Repository()
 SHORT_URL_LENGTH = 7
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
@@ -20,9 +22,22 @@ def hello():
 @app.route('/create')
 def create_url():
     url = request.args.get("url")
-    short_url = generate_short_url(SHORT_URL_LENGTH)
-    repository.add_url(url, short_url)
-    return short_url
+    id = generate_short_url(SHORT_URL_LENGTH)
+    repository.add_url(url, id)
+
+    return jsonify(id=id)
+
+
+@app.route('/<id>')
+def get_url_new(id):
+    url = repository.get_url(id)
+    dt = datetime.now()
+    ip = request.remote_addr
+
+    Thread(target=repository.increment_visit, args=(id,)).start()
+    Thread(target=repository.insert_ip_address_and_its_visit, args=(ip, id, dt))
+
+    return redirect(url)
 
 
 @app.route("/get")
